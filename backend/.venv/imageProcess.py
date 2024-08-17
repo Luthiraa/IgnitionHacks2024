f9ee9534bcf22f90bbc6d8a9fc4209e0824d55f7
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2024-08-17 11:58:35 -0400

    comments

--- backend/.venv/imageProcess.py
+++ backend/.venv/imageProcess.py
@@ -2,19 +2,15 @@ import os
 from google.cloud import vision
 from google.cloud.vision_v1 import types
 
-# Set the environment variable for the service account key
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'./keys/GCVAuthKey.json'
 
 def convert_image_to_latex(image_path):
-    # Initialize the Vision API client
     client = vision.ImageAnnotatorClient()
 
     # Load the image
     with open(image_path, 'rb') as image_file:
         content = image_file.read()
     image = types.Image(content=content)
-
-    # Perform text detection
     response = client.document_text_detection(image=image)
     annotations = response.full_text_annotation
     detected_text = annotations.text
@@ -23,12 +19,13 @@ def convert_image_to_latex(image_path):
     return latex_text
 
 def convert_to_latex(text):
-
     latex_text = text.replace('\n', ' \\\\ ')
     return latex_text
 
-# Example usage
 image_path = './images/test-image.png'
 latex_text = convert_image_to_latex(image_path)
-print(latex_text)
 
+# write to a LaTeX file in the same directory with utf-8 encoding
+with open('output.tex', 'w', encoding='utf-8') as f:
+    for line in latex_text.split('\n'):
+        f.write(line + '\n')
