6974b5bf4d43a7b622d6d4dfb48184929bfd00db
Ninjtheturtle <nengjiali@gmail.com>
2024-08-18 04:31:19 -0400

    signin

--- frontend/ignition-hacks/src/components/Signup.jsx
+++ frontend/ignition-hacks/src/components/Signup.jsx
@@ -71,70 +71,59 @@ const Signup = () => {
     };
 
     return (
-        <>
-
-            <div className="section">
-                <div className="container">
-                    <div className="row full-height justify-content-center">
-                        <div className="col-12 text-center align-self-center py-5">
-                            <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                                <h6 className="mb-0 pb-3">
-                                    <span>Log In / </span><span>Sign Up</span>
-                                </h6>
-                                <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" checked={isLogin} onChange={() => setIsLogin(!isLogin)} />
-                                <label htmlFor="reg-log"></label>
-                                <div className="card-3d-wrap mx-auto">
-                                    <div className="card-3d-wrapper">
-                                        <div className="card-front">
-                                            <div className="center-wrap">
-                                                <div className="section text-center">
-                                                    <h4 className="mb-4 pb-3">Log In</h4>
-                                                    <div className="form-group">
-                                                        <input type="text" name="logemail" className="form-style" placeholder="Your Username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" />
-                                                        <i className="input-icon uil uil-user"></i>
-                                                    </div>
-                                                    <div className="form-group mt-2">
-                                                        <input type="password" name="logpass" className="form-style" placeholder="Your Password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="off" />
-                                                        <i className="input-icon uil uil-lock-alt"></i>
-                                                    </div>
-                                                    <button className="btn mt-4" onClick={handleLogin}>submit</button>
-                                                    <p className="mb-0 mt-4 text-center"><a href="#0" className="link">Forgot your password?</a></p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="card-back">
-                                            <div className="center-wrap">
-                                                <div className="section text-center">
-                                                    <h4 className="mb-4 pb-3">Sign Up</h4>
-                                                    <div className="form-group">
-                                                        <input type="text" name="logname" className="form-style" placeholder="Your Username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" />
-                                                        <i className="input-icon uil uil-user"></i>
-                                                    </div>
-                                                    <div className="form-group mt-2">
-                                                        <input type="email" name="logemail" className="form-style" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off" />
-                                                        <i className="input-icon uil uil-at"></i>
-                                                    </div>
-                                                    <div className="form-group mt-2">
-                                                        <input type="password" name="logpass" className="form-style" placeholder="Your Password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="off" />
-                                                        <i className="input-icon uil uil-lock-alt"></i>
-                                                    </div>
-                                                    <div className="form-group mt-2">
-                                                        <input type="password" name="confirmpass" className="form-style" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} autoComplete="off" />
-                                                        <i className="input-icon uil uil-lock-alt"></i>
-                                                    </div>
-                                                    <button className="btn mt-4" onClick={handleSignup}>submit</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                {alertMessage && <p className="alert">{alertMessage}</p>}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div className={`container ${isLogin ? 'login' : 'signup'}`}>
+                <h2>{isLogin ? 'Login' : 'Signup'}</h2>
+                <form onSubmit={isLogin ? handleLogin : handleSignup}>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    {!isLogin && (
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    )}
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {!isLogin && (
+                        <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    )}
+                    <button type="submit">{isLogin ? 'Login' : 'Signup'}</button>
+                </form>
+                {alertMessage && <p className="alert">{alertMessage}</p>}
+                <p>
+                    {isLogin ? (
+                        <>
+                            Don't have an account?{' '}
+                            <button onClick={() => setIsLogin(false)}>Signup</button>
+                        </>
+                    ) : (
+                        <>
+                            Already have an account?{' '}
+                            <button onClick={() => setIsLogin(true)}>Login</button>
+                        </>
+                    )}
+                </p>
             </div>
-        </>
+        </div>
     );
 };
 
