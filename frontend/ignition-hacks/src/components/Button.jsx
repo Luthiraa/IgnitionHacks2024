f4f9a5169275b38de23f1dcb03007986354a25e4
Udula Abeykoon <udulaabeykoon@gmail.com>
2024-08-17 20:21:34 -0400

    updatedlandingpage

--- frontend/ignition-hacks/src/components/Button.jsx
+++ frontend/ignition-hacks/src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Button.css';
 
 const Button = ({ targetPage, children }) => {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const buttonElement = document.querySelector('.button');
+        buttonElement.classList.add('fade-in');
+    }, []);
 
     const handleClick = () => {
         const buttonElement = document.querySelector('.button');
